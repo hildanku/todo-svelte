ff24067e84a5f1f55293279120012051f61b1a7f
hildanku <hildankutomo@gmail.com>
2024-11-17 00:45:14 +0700

    fix(supabase.client): fix err and refactor export method

--- src/supabase.client.ts
+++ src/supabase.client.ts
@@ -1,8 +1,6 @@
-import createClient from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseApiKey = import.meta.env.VITE_SUPABASE_API_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseApiKey);
-
-export default supabase;
+export const supabase = createClient(supabaseUrl, supabaseApiKey);
